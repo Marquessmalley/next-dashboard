c0509aa7c065dd542b7878becef288fd72c74d22
Marques  Smalley <marquessmalley@Marquess-MBP.attlocal.net>
2025-03-14 23:05:44 -0400

    first push

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Doto, Lusitana } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+export const doto = Doto({ weight: "900", variable: "--", subsets: ["latin"] });
